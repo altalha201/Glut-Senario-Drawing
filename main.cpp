2905d10716a1d010e7908d7a7c94987b0acd5945
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-05-24 10:58:59 +0600

    some Progress

--- main.cpp
+++ main.cpp
@@ -4,11 +4,31 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
-void display(void)
+void ground()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3ub(153, 153, 102);
+    glBegin(GL_QUADS);
+    glVertex2d(1140, 450);
+    glVertex2d(1140, 420);
+    glVertex2d(960, 360);
+    glVertex2d(960, 390);
+    glEnd();
 
-    glColor3ub(165, 255, 165);
+    glBegin(GL_QUADS);
+    glVertex2d(1080, 240);
+    glVertex2d(1080, 210);
+    glVertex2d(960, 150);
+    glVertex2d(960, 180);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glVertex2d(1080, 150);
+    glVertex2d(1080, 120);
+    glVertex2d(990, 0);
+    glVertex2d(960, 0);
+    glEnd();
+
+    glColor3ub(34, 139, 34);
     glBegin(GL_POLYGON);
     glVertex2d(0, 480);
     glVertex2d(960, 480);
@@ -22,7 +42,40 @@ void display(void)
     glVertex2d(0,0);
     glVertex2d(0, 480);
     glEnd();
+}
 
+void sky()
+{
+    glBegin(GL_QUADS);
+    glColor3ub(51, 204, 255);//1st color
+    glVertex2d(0, 840);
+    glVertex2d(1920, 840);
+    glColor3ub( 0, 102, 204);//2nd color
+    glVertex2d(1920, 1080);
+    glVertex2d(0, 1080);
+    glEnd();
+}
+
+void river()
+{
+    glBegin(GL_QUADS);
+    glColor3ub(0, 76, 153);
+    glVertex2d(0, 840);
+    glColor3ub(0, 128, 255);
+    glVertex2d(1920, 840);
+    glColor3ub(102, 178, 255);
+    glVertex2d(1920, 0);
+    glVertex2d(0, 0);
+    glEnd();
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    river();
+    ground();
+    sky();
 
     glFlush();
 }
